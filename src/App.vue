7842603eeed49693becf86832e6348451d8d39c5
Austin Bennett <bboywake@gmail.com>
2019-10-24 22:31:14 -0600

    Fixed some formatting and added ability to add players

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
 	<div id="app">
-		<div id="nav">
-			<router-link to="/">Home</router-link>|
-			<router-link to="/about">About</router-link>|
-			<router-link to="/players">Players</router-link>
-		</div>
-		<router-view />
+		<v-app>
+			<div id="nav">
+				<router-link to="/">Home</router-link>|
+				<router-link to="/about">About</router-link>|
+				<router-link to="/players">Players</router-link>
+			</div>
+			<router-view />
+		</v-app>
 	</div>
 </template>
 
--- src/views/Players.vue
+++ src/views/Players.vue
@@ -2,18 +2,31 @@
 <template>
 	<v-container class="grey lighten-5">
 		<v-row no-gutters>
-			<v-col cols="12" sm="6" md="8">
-				<v-card>
-					<v-text-field class="player-input" v-model="playerName" label="Player Name"></v-text-field>
-					<v-button @click="addPlayer">Add Player</v-button>
+			<v-col cols="12">
+				<v-card class="pa-4">
+					<transition>
+						<div class="error-message">{{ errorMessage }}</div>
+					</transition>
+					<ol class="players-list text-left">
+						<li v-for="player in players" :key="player.id" class="player-name">
+							{{ player.name }}
+							<span class="remove-player">Remove</span>
+						</li>
+					</ol>
+					<v-row>
+						<v-col cols="12" sm="9">
+							<v-text-field class="player-input" v-model="playerName" label="Player Name"></v-text-field>
+						</v-col>
+						<v-col cols="12" sm="3" class="d-flex align-center">
+							<v-btn @click="addPlayer" block class="my-1 primary">Add Player</v-btn>
+						</v-col>
+					</v-row>
 				</v-card>
-				<v-card>
-					<div class="players-list">
-						<ol>
-							<li v-for="player in players" :key="player.id"></li>
-						</ol>
-					</div>
-				</v-card>
+				<v-card></v-card>
+				<!-- <div>
+					<h3>{{ playerName }}</h3>
+					<h4>Players: {{ players }}</h4>
+				</div>-->
 			</v-col>
 		</v-row>
 	</v-container>
@@ -26,20 +39,48 @@ export default {
 			playerId: 1,
 			playerName: '',
 			players: [],
+			showError: false,
+			errorMessage: '',
 		};
 	},
 	methods: {
 		addPlayer() {
-			this.players.push({
-				id: this.playerId,
-				name: this.playerName,
-			});
-			this.playerId += 1;
-			this.playerName = '';
+			this.clearError();
+			if (this.playerName.length > 1) {
+				this.players.push({
+					id: this.playerId,
+					name: this.playerName,
+				});
+				this.playerId += 1;
+				this.playerName = '';
+			} else {
+				this.error('A name must be at least 2 characters');
+			}
+		},
+		error(message) {
+			this.errorMessage = message;
+			this.showError = true;
+		},
+		clearError() {
+			this.showError = false;
 		},
 	},
 };
 </script>
 
-<style scss>
+<style lang="scss">
+.players-list {
+	:nth-child(n + 1) {
+		background-color: #888;
+	}
+}
+.player-name {
+	position: relative;
+	.remove-player {
+		position: absolute;
+		right: 0;
+		cursor: pointer;
+		color: blue;
+	}
+}
 </style>
