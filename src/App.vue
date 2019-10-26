78f40ac6e297790888512e6683b0d0d830967495
Austin Bennett <bboywake@gmail.com>
2019-10-25 20:19:36 -0600

    Fixed some stuff and added an edit link to the player list, still needs to be functional

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@
 </template>
 
 <style lang="scss">
+html {
+	font-size: 62.5%;
+}
 #app {
 	font-family: "Avenir", Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
--- src/views/Players.vue
+++ src/views/Players.vue
@@ -7,20 +7,25 @@
 					<transition>
 						<div class="error-message">{{ errorMessage }}</div>
 					</transition>
-					<ol class="players-list text-left">
-						<li v-for="player in players" :key="player.id" class="player-name">
+					<ol class="players-list text-left pa-0">
+						<li v-for="player in players" :key="player.id" class="player-name pa-3">
 							{{ player.name }}
-							<span class="remove-player">Remove</span>
+							<div class="player-control">
+								<span class="edit-player">Edit</span>
+								<span class="remove-player">Remove</span>
+							</div>
 						</li>
 					</ol>
-					<v-row>
-						<v-col cols="12" sm="9">
-							<v-text-field class="player-input" v-model="playerName" label="Player Name"></v-text-field>
-						</v-col>
-						<v-col cols="12" sm="3" class="d-flex align-center">
-							<v-btn @click="addPlayer" block class="my-1 primary">Add Player</v-btn>
-						</v-col>
-					</v-row>
+					<v-form @submit.prevent="addPlayer">
+						<v-row>
+							<v-col cols="12" sm="9">
+								<v-text-field class="player-input" v-model="playerName" label="Player Name"></v-text-field>
+							</v-col>
+							<v-col cols="12" sm="3" class="d-flex align-center">
+								<v-btn type="submit" block class="my-1 primary">Add Player</v-btn>
+							</v-col>
+						</v-row>
+					</v-form>
 				</v-card>
 				<v-card></v-card>
 				<!-- <div>
@@ -70,17 +75,25 @@ export default {
 
 <style lang="scss">
 .players-list {
-	:nth-child(n + 1) {
-		background-color: #888;
+	list-style: none;
+	padding: 0;
+	& > :nth-child(even) {
+		background-color: #eee;
 	}
 }
 .player-name {
 	position: relative;
-	.remove-player {
+	.player-control {
+		display: inline-block;
 		position: absolute;
-		right: 0;
-		cursor: pointer;
-		color: blue;
+		right: 1.2rem;
+		span {
+			cursor: pointer;
+			color: blue;
+			&:last-child {
+				margin-left: 1rem;
+			}
+		}
 	}
 }
 </style>
