8896536c1e3824a2b7ca14af3ed6b6c6e516bc55
Austin Bennett <austin@skaduusch.com>
2019-10-28 22:59:44 -0700

    Added ability to remove a player. Working on ability to edit a player name

--- src/views/Players.vue
+++ src/views/Players.vue
@@ -8,18 +8,43 @@
 						<div class="error-message">{{ errorMessage }}</div>
 					</transition>
 					<ol class="players-list text-left pa-0">
-						<li v-for="player in players" :key="player.id" class="player-name pa-3">
-							{{ player.name }}
-							<div class="player-control">
-								<span class="edit-player">Edit</span>
-								<span class="remove-player">Remove</span>
-							</div>
-						</li>
+						<transition-group>
+							<li
+								v-for="(player, index) in players"
+								:key="player.id"
+								class="player-name pa-3"
+							>
+								{{ player.name }}
+								<v-text-field
+									@keyup.enter="doneEdit(player, index)"
+									@keyup.esc="cancelEdit(index)"
+									:value="player.name"
+								/>
+								<v-btn
+									@click="doneEdit(player, index)"
+									@keyup.enter="doneEdit(player, index)"
+									@keyup.esc="cancelEdit(index)"
+									>Change</v-btn
+								>
+								<div class="player-control">
+									<span class="edit-player" @click="editPlayer(player)"
+										>Edit</span
+									>
+									<span class="remove-player" @click="removePlayer(index)"
+										>Remove</span
+									>
+								</div>
+							</li>
+						</transition-group>
 					</ol>
 					<v-form @submit.prevent="addPlayer">
 						<v-row>
 							<v-col cols="12" sm="9">
-								<v-text-field class="player-input" v-model="playerName" label="Player Name"></v-text-field>
+								<v-text-field
+									class="player-input"
+									v-model="playerName"
+									label="Player Name"
+								></v-text-field>
 							</v-col>
 							<v-col cols="12" sm="3" class="d-flex align-center">
 								<v-btn type="submit" block class="my-1 primary">Add Player</v-btn>
@@ -44,6 +69,7 @@ export default {
 			playerId: 1,
 			playerName: '',
 			players: [],
+			editedPlayer: {},
 			showError: false,
 			errorMessage: '',
 		};
@@ -61,6 +87,28 @@ export default {
 			} else {
 				this.error('A name must be at least 2 characters');
 			}
+		},
+		removePlayer(index) {
+			this.players.splice(index, 1);
+		},
+		editPlayer(player) {
+			this.playerNameCache = player.name;
+			this.editedPlayer = player;
+		},
+		doneEdit(player, index) {
+			if (!this.editedPlayer) {
+				return;
+			}
+			this.editedPlayer = null;
+			this.players[index].name = player.name.trim();
+			// player1.name = player.name.trim();
+			if (!player.name.trim()) {
+				this.removePlayer(index);
+			}
+		},
+		cancelEdit(index) {
+			this.editedPlayer = null;
+			this.players[index].name = this.playerNameCache;
 		},
 		error(message) {
 			this.errorMessage = message;
